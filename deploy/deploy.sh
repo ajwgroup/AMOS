bcc7af089b9fa454ee5226f890850136f033a915
Mike Bolland <mike.bolland@ajw-group.com>
2018-11-20 10:55:21 +0000

    remove list

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,6 +4,5 @@ ts=$(date +"%y%m%d%H%M")
 echo "Starting pack"
 dotnet pack AMOS/AMOS.csproj /p:PackageVersion=1.0.$ts --configuration Release
 echo "Starting push"
-dotnet nuget push --help
 dotnet nuget push AMOS/bin/Release/*1.0.$ts.nupkg --source https://api.nuget.org/v3/index.json --api-key $ApiKey
 
