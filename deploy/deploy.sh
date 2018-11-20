ed342a816251f08a6bd34ddd7bb7465830930fe0
Mike Bolland <mike.bolland@ajw-group.com>
2018-11-20 10:05:57 +0000

    remove detail

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,5 @@
 ApiKey=$1
 ts=$(date +"%y%m%d%H%M")
 
-dotnet pack AMOS/AMOS.csproj /p:PackageVersion=1.0.$ts --verbosity detailed --configuration Release
+dotnet pack AMOS/AMOS.csproj /p:PackageVersion=1.0.$ts --configuration Release
 dotnet nuget push AMOS/bin/Release/*1.0.$ts.nupkg --api-key $ApiKey --source https://api.nuget.org/v3/index.json
