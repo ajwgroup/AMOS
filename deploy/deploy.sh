c3acb2e314b933e245cb23b1f11d773ea2cec075
Mike Bolland <mike.bolland@ajw-group.com>
2018-11-20 10:11:25 +0000

    some verbosity

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,8 @@
 ApiKey=$1
 ts=$(date +"%y%m%d%H%M")
 
+echo "Starting pack"
 dotnet pack AMOS/AMOS.csproj /p:PackageVersion=1.0.$ts --configuration Release
+echo "Starting push"
 dotnet nuget push AMOS/bin/Release/*1.0.$ts.nupkg --api-key $ApiKey --source https://api.nuget.org/v3/index.json
+
