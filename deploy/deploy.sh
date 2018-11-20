0240fba2437b0f271e8b91df9852e4dfa32d2518
Mike Bolland <mike.bolland@ajw-group.com>
2018-11-20 10:22:37 +0000

    dotnet nuget push seems to want a -Source parameter as if its nuget.exe

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,5 +4,5 @@ ts=$(date +"%y%m%d%H%M")
 echo "Starting pack"
 dotnet pack AMOS/AMOS.csproj /p:PackageVersion=1.0.$ts --configuration Release
 echo "Starting push"
-dotnet nuget push AMOS/bin/Release/*1.0.$ts.nupkg --api-key $ApiKey --source https://api.nuget.org/v3/index.json
+dotnet nuget push AMOS/bin/Release/*1.0.$ts.nupkg --api-key $ApiKey --source https://api.nuget.org/v3/index.json -Source https://api.nuget.org/v3/index.json
 
