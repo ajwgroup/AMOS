1a2cc77c086f40289e6b25beba89bedd723fec55
Mike Bolland <mike.bolland@ajw-group.com>
2019-11-05 15:20:49 +0000

    add transfer part defintiion

--- AMOSTests/UnitTest1.cs
+++ AMOSTests/UnitTest1.cs
@@ -2,6 +2,7 @@ using AMOS;
 using AMOS.Models.AMOS_TRANSPORT_ENVELOPE.v0_1;
 using AMOS.Models.TRANSFER_ORDER.v0_14;
 using AMOS.Models.TRANSFER_PART.v0_14;
+using AMOS.Models.TRANSFER_PART_DEFINITION.v1_1;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
@@ -18,49 +19,49 @@ namespace AMOSTests
         [TestMethod]
         public void TransferOrderFromXml_PassesFile_ReturnsExpectedValueForOrder()
         {
-            var transportEnvelope = EnvelopeUtils.FromXml<transportEnvelope_0_1>(Encoding.UTF8.GetBytes(File.ReadAllText("TRANSFER_ORDER.xml")));
+            var transportEnvelope = EnvelopeUtils.FromXml<transportEnvelope_0_1>(Encoding.UTF8.GetBytes(File.ReadAllText("TRANSFER_ORDER_0_14.xml")));
             Assert.AreEqual("R11111111", transportEnvelope.getPayload<transferOrder_0_14>().order.First().orderNumber);
         }
 
         [TestMethod]
         public void TransferOrderFromXmlOrderHeader_PassesFile_ReturnsExpectedValueForOrder()
         {
-            var transportEnvelope = EnvelopeUtils.FromXml<transportEnvelope_0_1>(Encoding.UTF8.GetBytes(File.ReadAllText("TRANSFER_ORDER.xml")));
+            var transportEnvelope = EnvelopeUtils.FromXml<transportEnvelope_0_1>(Encoding.UTF8.GetBytes(File.ReadAllText("TRANSFER_ORDER_0_14.xml")));
             Assert.AreEqual("EXW", transportEnvelope.getPayload<transferOrder_0_14>().order.First().orderHeader.deliveryCondition.code);
         }
 
         [TestMethod]
         public void TransferOrderFromXmlPart_PassesFile_ReturnsExpectedValueForOrder()
         {
-            var transportEnvelope = EnvelopeUtils.FromXml<transportEnvelope_0_1>(Encoding.UTF8.GetBytes(File.ReadAllText("TRANSFER_ORDER.xml")));
+            var transportEnvelope = EnvelopeUtils.FromXml<transportEnvelope_0_1>(Encoding.UTF8.GetBytes(File.ReadAllText("TRANSFER_ORDER_0_14.xml")));
             Assert.AreEqual("D31865-111", transportEnvelope.getPayload<transferOrder_0_14>().order.First().orderDetail.First().partNumber.Value);
         }
 
         [TestMethod]
         public void TransferOrderFromXmlAircraftReg_PassesFile_ReturnsExpectedValueForOrder()
         {
-            var transportEnvelope = EnvelopeUtils.FromXml<transportEnvelope_0_1>(Encoding.UTF8.GetBytes(File.ReadAllText("TRANSFER_ORDER.xml")));
+            var transportEnvelope = EnvelopeUtils.FromXml<transportEnvelope_0_1>(Encoding.UTF8.GetBytes(File.ReadAllText("TRANSFER_ORDER_0_14.xml")));
             Assert.AreEqual("Z-ZZZZ", transportEnvelope.getPayload<transferOrder_0_14>().order.First().orderDetail.First().aircraftRegistration);
         }
 
         [TestMethod]
         public void TransferOrderFromXmlTargetDate_PassesFile_ReturnsExpectedValueForOrder()
         {
-            var transportEnvelope = EnvelopeUtils.FromXml<transportEnvelope_0_1>(Encoding.UTF8.GetBytes(File.ReadAllText("TRANSFER_ORDER.xml")));
+            var transportEnvelope = EnvelopeUtils.FromXml<transportEnvelope_0_1>(Encoding.UTF8.GetBytes(File.ReadAllText("TRANSFER_ORDER_0_14.xml")));
             Assert.AreEqual("2018-06-11", transportEnvelope.getPayload<transferOrder_0_14>().order.First().orderDetail.First().targetDate);
         }
 
         [TestMethod]
         public void TransferOrderFromXmlTargetDateConvetToDateTime_PassesFile_ReturnsExpectedValueForOrder()
         {
-            var transportEnvelope = EnvelopeUtils.FromXml<transportEnvelope_0_1>(Encoding.UTF8.GetBytes(File.ReadAllText("TRANSFER_ORDER.xml")));
+            var transportEnvelope = EnvelopeUtils.FromXml<transportEnvelope_0_1>(Encoding.UTF8.GetBytes(File.ReadAllText("TRANSFER_ORDER_0_14.xml")));
             Assert.AreEqual(DateTime.Parse("11/06/2018 00:00:00", new CultureInfo("en-GB")), DateTime.ParseExact(transportEnvelope.getPayload<transferOrder_0_14>().order.First().orderDetail.First().targetDate, "yyyy-MM-dd", null));
         }
 
         [TestMethod]
         public void TransferOrderFromXmlAddressing_PassesFile_ReturnsExpectedValueForOrder()
         {
-            var transportEnvelope = EnvelopeUtils.FromXml<transportEnvelope_0_1>(Encoding.UTF8.GetBytes(File.ReadAllText("TRANSFER_ORDER.xml")));
+            var transportEnvelope = EnvelopeUtils.FromXml<transportEnvelope_0_1>(Encoding.UTF8.GetBytes(File.ReadAllText("TRANSFER_ORDER_0_14.xml")));
             Assert.AreEqual("AJWMAIN", transportEnvelope.getPayload<transferOrder_0_14>().order.First().orderHeader.addressing.shipmentAddress.code);
         }
 
@@ -69,6 +70,13 @@ namespace AMOSTests
         {
             var transportEnvelope = EnvelopeUtils.FromXml<transportEnvelope_0_1>(Encoding.UTF8.GetBytes(File.ReadAllText("TRANSFER_PART_D31865-111.xml")));
             Assert.AreEqual("D31865-111", transportEnvelope.getPayload<transferPart_0_14>().part.First().partNumber);
+        }
+
+        [TestMethod]
+        public void TransferPartDefintionFromXml_PassesFile_ReturnsExpectedValueForOrder()
+        {
+            var transportEnvelope = EnvelopeUtils.FromXml<transportEnvelope_0_1>(Encoding.UTF8.GetBytes(File.ReadAllText("TRANSFER_PART_DEFINITION_6773E010000.xml")));
+            Assert.AreEqual("6773E010000", transportEnvelope.getPayload<transferPartDefinition_1_1>().part.First().partNumber);
         }
     }
 }
