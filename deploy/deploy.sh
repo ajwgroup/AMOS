e70a2fcbfbf066aa4af8e06c32d4724d0e01ade4
Mike Bolland <mike.bolland@ajw-group.com>
2022-03-04 13:38:35 +0000

    use year as version

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,8 +1,8 @@
 ApiKey=$1
-ts=$(date +"%y%m%d%H%M")
+ts=$(date +"%Y.%m.%d.%H%M")
 
 echo "Starting pack"
-dotnet pack AMOS/AMOS.csproj /p:PackageVersion=1.0.$ts --configuration Release
+dotnet pack AMOS/AMOS.csproj /p:PackageVersion=$ts --configuration Release
 echo "Starting push"
 dotnet nuget push AMOS/bin/Release/*1.0.$ts.nupkg --source https://api.nuget.org/v3/index.json --api-key $ApiKey
 
