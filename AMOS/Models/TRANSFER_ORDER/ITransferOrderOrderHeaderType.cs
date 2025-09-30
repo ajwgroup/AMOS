b40f39fee9b0adb0104cd383403df0126b703861
Mike Bolland <Mike.Bolland@ajw-group.com>
2025-09-30 16:57:45 +0100

    amend orderState

--- AMOS/Models/TRANSFER_ORDER/ITransferOrderOrderHeaderType.cs
+++ AMOS/Models/TRANSFER_ORDER/ITransferOrderOrderHeaderType.cs
@@ -7,18 +7,6 @@ namespace AMOS.Models.TRANSFER_ORDER
     public class ITransferOrderOrderHeaderType
     {
 
-        /// <remarks/>
-        [System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
-        public string orderState
-        {
-            get
-            {
-                return this.orderStateField;
-            }
-            set
-            {
-                this.orderStateField = value;
-            }
-        }
+        public string orderState { get; set; }
     }
 }
