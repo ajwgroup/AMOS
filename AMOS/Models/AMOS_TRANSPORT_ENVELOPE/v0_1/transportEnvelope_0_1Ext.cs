65328a817c3bbfdbf2d1d60e935b9b066446f31c
Mike Bolland <Mike.Bolland@ajw-group.com>
2025-10-02 11:43:12 +0100

    Add missing payloads

--- AMOS/Models/AMOS_TRANSPORT_ENVELOPE/v0_1/transportEnvelope_0_1Ext.cs
+++ AMOS/Models/AMOS_TRANSPORT_ENVELOPE/v0_1/transportEnvelope_0_1Ext.cs
@@ -1,4 +1,6 @@
-﻿using AMOS.Models.TRANSFER_ORDER.v1_2;
+﻿using AMOS.Models.AMOS_TRANSPORT_ENVELOPE.v1_0;
+using AMOS.Models.AMOS_TRANSPORT_ENVELOPE.v1_1;
+using AMOS.Models.TRANSFER_ORDER.v1_2;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -10,6 +12,28 @@ namespace AMOS.Models.AMOS_TRANSPORT_ENVELOPE.v0_1
     public static class transportEnvelope_0_1Ext
     {
         public static T GetPayload<T>(this transportEnvelope_0_1 envelope)
+        {
+            var doc = new System.Xml.XmlDocument();
+            foreach (var node in envelope.payload.Any)
+                doc.AppendChild(doc.ImportNode(node, true));
+
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            using (StringReader stringreader = new StringReader(doc.OuterXml))
+                return (T)serializer.Deserialize(stringreader);
+        }
+
+        public static T GetPayload<T>(this transportEnvelope_1_0 envelope)
+        {
+            var doc = new System.Xml.XmlDocument();
+            foreach (var node in envelope.payload.Any)
+                doc.AppendChild(doc.ImportNode(node, true));
+
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            using (StringReader stringreader = new StringReader(doc.OuterXml))
+                return (T)serializer.Deserialize(stringreader);
+        }
+
+        public static T GetPayload<T>(this transportEnvelope_1_1 envelope)
         {
             var doc = new System.Xml.XmlDocument();
             foreach (var node in envelope.payload.Any)
